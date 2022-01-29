2f4912e8cde669d3a0900d2f3037f1222d5e142e
Brick <powerzieeee@gmail.com>
2022-01-30 00:01:27 +0200

    Some Logic

--- GGJGame/Assets/SRC/PlayerLogic/AppleQuestLogic.cs
+++ GGJGame/Assets/SRC/PlayerLogic/AppleQuestLogic.cs
@@ -1,30 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class PlayerQuestsLogic : MonoBehaviour
-{
-
-    
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-}
 
 public class AppleQuestLogic : MonoBehaviour
 {
     public bool QuestIsEnded { get; set; } =false;
     public int countOfAppleToBring = 10;
     private int currentCountOfApples = 0;
-    private void OnCollisionEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
-        if(other.tag=="Apple"&&!QuestIsEnded)
+        if(other.gameObject.tag=="Apple"&&!QuestIsEnded)
         {
             Destroy(other.gameObject);
             if ( currentCountOfApples++ ==countOfAppleToBring)
